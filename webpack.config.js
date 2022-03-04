9d2d832959ecca76c4b07111728d4a88e459ec55
jsaurav7 <lynxrenb@gmail.com>
2022-03-05 01:09:30 +0530

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const PORT = 3001
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/app.js'),
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: PORT
+  },
+  output: {
+    filename: 'survezy.js',
+    library: {
+      type: 'umd',
+      name: 'survezy'
+    },
+    // prevent error: `Uncaught ReferenceError: self is not define`
+    globalObject: 'this'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack', 'url-loader']
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin()]
+}
