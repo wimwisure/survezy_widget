bdd4843e231d572f54fcaa624bcc63912de90fee
jsaurav7 <lynxrenb@gmail.com>
2022-03-15 12:18:15 +0530

    update widget

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Survezy } from './index'
 
-export const Show = (link) => {
+export const Show = (link, darkMode) => {
   // eslint-disable-next-line no-unused-expressions
   document.getElementById('survezy_container')?.remove()
   const div = document.createElement('div')
@@ -12,7 +12,7 @@ export const Show = (link) => {
 
   ReactDOM.render(
     <React.StrictMode>
-      <Survezy link={link} />
+      <Survezy link={link} darkMode={darkMode} />
     </React.StrictMode>,
     div
   )
