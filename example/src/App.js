b872d55bab19299c9724570a9009c884f2900e7f
jsaurav7 <lynxrenb@gmail.com>
2022-03-16 23:44:05 +0530

    fix bug

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { Survezy } from 'survezy_widget'
 import 'survezy_widget/dist/index.css'
 
 const App = () => {
-  return <Survezy link={"cml0aWtrcjI0MDVAZ21haWwuY29tIHN1cnZlenlfbG9naW5QYWdl"} />
+  return <Survezy link={"c2F1cmF2QHdpbXdpc3VyZS5jb20gY29udA=="} />
 }
 
 export default App
