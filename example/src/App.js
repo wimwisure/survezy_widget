dd35ef22566c9dff8791422593715b49c99f4574
jsaurav7 <lynxrenb@gmail.com>
2022-04-14 12:34:31 +0530

    fix bug

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Survezy } from 'survezy_widget'
 import 'survezy_widget/dist/index.css'
 
 const App = () => {
-
-  const demoSurvey2 ={
+  const demoSurvey2 = {
     questions: [
       {
         id: 1649417569002,
@@ -18,10 +17,9 @@ const App = () => {
         id: 1649417569002,
         text: 'DROPDWON 2',
         type: 'DROPDOWN',
-        options: [{
-          id: 1,
-          value: 'a'
-        }],
+        options: [
+          "GOOD 2", "FINE 2", "BAD 2"
+        ],
         required: true,
       }
     ]
@@ -31,29 +29,38 @@ const App = () => {
   const demoSurvey = {
     questions: [
       {
-       text: "How's Your day going",
-       type: "DROPDOWN",
-       options: [
-         "GOOD", "FINE", "BAD"
-       ]
+        text: "How's Your day going",
+        type: "DROPDOWN",
+        options: [
+          "GOOD", "FINE", "BAD"
+        ]
       },
       {
         text: "Product Review",
         type: "RATING",
         options: []
       },
-       {
+      {
         text: "DROPDOWN 3",
         type: "DROPDOWN",
         options: [
           "Excellent", "FINE", "BAD"
         ]
-       },
+      },
 
     ]
   }
 
-  return <Survezy eventId="c2F1cmF2QHdpbXdpc3VyZS5jb20gU3VydmV6eV9VWA==" />
+  const [survey, setSurvey] = useState(demoSurvey)
+
+  return (
+    <div>
+      <button onClick={() => setSurvey(demoSurvey2)}>
+        Set Survey
+      </button>
+      <Survezy demoSurvey={survey} />
+    </div>
+  )
 }
 
 export default App
