0ab3ca01e935ea4b0840d6243b346c2e9e25c7b4
jsaurav7 <lynxrenb@gmail.com>
2022-05-10 13:54:33 +0530

    fix bugs

--- example/src/App.js
+++ example/src/App.js
@@ -4,64 +4,18 @@ import { Survezy } from 'survezy_widget'
 import 'survezy_widget/dist/index.css'
 
 const App = () => {
-  const demoSurvey2 = {
-    currentIndex: 0,
-    questions: [
-      {
-        id: 1649417569002,
-        text: 'Rating 1',
-        type: 'RATING',
-        options: [],
-        required: true,
-      },
-      {
-        id: 1649417569002,
-        text: 'DROPDWON 2',
-        type: 'DROPDOWN',
-        options: [
-          "GOOD 2", "FINE 2", "BAD 2"
-        ],
-        required: true,
-      }
-    ]
-  }
-
-
-  const demoSurvey = {
-    currentIndex: 0,
-    questions: [
-      {
-        text: "How's Your day going",
-        type: "DROPDOWN",
-        options: [
-          "GOOD", "FINE", "BAD"
-        ]
-      },
-      {
-        text: "Product Review",
-        type: "RATING",
-        options: []
-      },
-      {
-        text: "DROPDOWN 3",
-        type: "DROPDOWN",
-        options: [
-          "Excellent", "FINE", "BAD"
-        ]
-      },
-
-    ]
-  }
-
-  const [survey, setSurvey] = useState(demoSurvey)
-
   return (
-    <div>
-      <button onClick={() => setSurvey(demoSurvey2)}>
-        Set Survey
-      </button>
-      <Survezy demoSurvey={survey} />
-    </div>
+    // <Survezy eventId={"c2F1cmF2QHdpbXdpc3VyZS5jb20gTmV3IEV2ZW50"} />
+    <Survezy path={"8ZKN"} />
+    // <Survezy demoSurvey={{
+    //   questions: [
+    //     {
+    //       text: "Question 1",
+    //       type: "SCALE",
+    //       required: true
+    //     }
+    //   ]
+    // }} />
   )
 }
 
